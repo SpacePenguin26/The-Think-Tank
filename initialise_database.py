02474e398f16d0c78de659d2fd6ae3c44e39c4da
SpacePenguin26 <109085915+SpacePenguin26@users.noreply.github.com>
2024-07-16 00:42:55 +0000

    Version: 0.0.7

--- initialise_database.py
+++ initialise_database.py
@@ -1,8 +1,8 @@
 # ---------- File Details --------------------------------------------------------------
 # Name: initialise_database.py
-# Version: 0.0.3
+# Version: 0.0.7
 # Date Created:  14.07.2024 - 23:13
-# Last Modified: 14.07.2024 - 23:52
+# Last Modified: 16.07.2024 - 01:27
 # --------------------------------------------------------------------------------------
 
 
@@ -101,18 +101,18 @@ cursor.execute('''
 
 # Define the list of questions with their details
 questions = [
-    (1, 'English', 'An Inspector Calls', 'Single Response', 'Who is the author of "An Inspector Calls"?', 1, None, None, None, None, 'J.B. Priestley'),
+    (1, 'English', 'An Inspector Calls', 'Single Response', 'Who is the author of "An Inspector Calls"?', 1, None, None, None, None, 'j.b. priestley'),
     (2, 'English', 'An Inspector Calls', 'Single Response', 'In which year is the play "An Inspector Calls" set?', 1, None, None, None, None, '1912'),
-    (3, 'English', 'An Inspector Calls', 'Single Response', 'Who is the mysterious inspector that arrives at the Birling household?', 1, None, None, None, None, 'Inspector Goole'),
-    (4, 'English', 'An Inspector Calls', 'Single Response', 'Which character is the daughter of Arthur and Sybil Birling?', 1, None, None, None, None, 'Sheila Birling'),
-    (5, 'English', 'An Inspector Calls', 'Multiple Choice', 'What is Arthur Birling\'s occupation?', 4, 'Doctor', 'Lawyer', 'Factory owner', 'Teacher', 'c'),
-    (6, 'English', 'An Inspector Calls', 'Multiple Choice', 'Where does Gerald Croft tell Sheila he was during the summer?', 4, 'On a business trip', 'At the coast', 'Visiting his family', 'In London', 'd'),
+    (3, 'English', 'An Inspector Calls', 'Single Response', 'Who is the mysterious inspector that arrives at the Birling household?', 1, None, None, None, None, 'inspector goole'),
+    (4, 'English', 'An Inspector Calls', 'Single Response', 'Which character is the daughter of Arthur and Sybil Birling?', 1, None, None, None, None, 'sheila'),
+    (5, 'English', 'An Inspector Calls', 'Multiple Choice', 'What is Arthur Birling\'s occupation?', 4, 'Doctor', 'Lawyer', 'Factory Owner', 'Teacher', 'c'),
+    (6, 'English', 'An Inspector Calls', 'Multiple Choice', 'Where does Gerald Croft tell Sheila he was during the summer?', 4, 'On a Business Trip', 'At the Coast', 'Visiting his Family', 'In London', 'd'),
     (7, 'English', 'An Inspector Calls', 'Multiple Choice', 'What is the inspector’s full name?', 4, 'Inspector Goole', 'Inspector Ghoul', 'Inspector Gould', 'Inspector Gale', 'a'),
     (8, 'English', 'An Inspector Calls', 'Multiple Choice', 'Which character is engaged to Sheila Birling?', 4, 'Eric Birling', 'Gerald Croft', 'Inspector Goole', 'Mr. Birling', 'b'),
-    (9, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following characters are part of the Birling family?', 4, 'Arthur', 'Eva', 'Sybil', 'Sheila', 'a,c,d'),
-    (10, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of these themes are explored in "An Inspector Calls"?', 4, 'Social responsibility', 'Capitalism', 'Gender inequality', 'Environmental issues', 'a,b,c'),
-    (11, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following actions were taken by members of the Birling family against Eva Smith?', 4, 'Firing her from a job', 'Denying her charity help', 'Evicting her from an apartment', 'Taking her to the hospital', 'a,b,c'),
-    (12, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following characters express remorse by the end of the play?', 4, 'Arthur Birling', 'Sheila Birling', 'Eric Birling', 'Gerald Croft', 'b,c'),
+    (9, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following characters are part of the Birling family?', 4, 'Arthur', 'Eva', 'Sybil', 'Sheila', 'a, c, d'),
+    (10, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of these themes are explored in "An Inspector Calls"?', 4, 'Social Responsibility', 'Capitalism', 'Gender Inequality', 'Environmental Issues', 'a, b, c'),
+    (11, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following actions were taken by members of the Birling family against Eva Smith?', 4, 'Firing her from a job', 'Denying her Charity Help', 'Evicting her from an Apartment', 'Taking her to the Hospital', 'a, b, c'),
+    (12, 'English', 'An Inspector Calls', 'Multi-Response', 'Which of the following characters express remorse by the end of the play?', 4, 'Arthur Birling', 'Sheila Birling', 'Eric Birling', 'Gerald Croft', 'b, c'),
     (13, 'English', 'An Inspector Calls', 'True or False', '"An Inspector Calls" is set in 1912.', 1, None, None, None, None, 'True'),
     (14, 'English', 'An Inspector Calls', 'True or False', 'Eva Smith is revealed to be related to Inspector Goole.', 1, None, None, None, None, 'False'),
     (15, 'English', 'An Inspector Calls', 'True or False', 'Sheila Birling initially tries to deny any involvement in Eva Smith\'s life.', 1, None, None, None, None, 'True'),
